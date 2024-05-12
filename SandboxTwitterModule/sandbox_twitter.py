1448cd2e4e1c08eafae86698a2ea05734e869e3e
cjs <cjs@bupt.edu.cn>
2024-05-12 12:09:17 +0800

    agent creating has been automated(

--- SandboxTwitterModule/sandbox_twitter.py
+++ SandboxTwitterModule/sandbox_twitter.py
@@ -29,24 +29,10 @@ class SandboxTwitter:
         # Simulate agent activities
         self.agents_graph.simulate_agent_activities(num_activities=10)
 
-<<<<<<< HEAD
-        # response = await self.channel.read_from_send_queue(message_id)
-        # await self.channel.write_to_receive_queue((None, None, "exit"))
-        # # 发送一个动作到Twitter实例并等待响应
-
-        # print(f"Received response: {response}")
-
-        test_agent = TwitterUserAgent(1, 'Alice', self.channel)
-        await test_agent.action_sign_up("alice0101", "Alice", "A girl.")
-        await test_agent.action_create_tweet("hello world")
-
-        # 发送退出信号以结束running方法
-=======
         # Wait for all tasks to complete
         await asyncio.gather(*[task for task in asyncio.all_tasks() if task is not asyncio.current_task()])
 
         # Send an exit signal to gracefully end the `running` method
->>>>>>> c4bedce75bb9ab6f97abe3e6526a43fab6b513e5
         await self.channel.write_to_receive_queue((None, None, "exit"))
         await task
         print("Simulation completed.")
