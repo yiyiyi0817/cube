b4bfce8f1593bea0f98f80acf65c0188927cc746
cjs <cjs@bupt.edu.cn>
2024-05-18 18:20:16 +0800

    Revert "correct mosts import errors"
    This reverts commit 79e6c5607a12e4b17fe7de025286732a99ccd6df.

--- test/conftest.py
+++
@@ -1,8 +0,0 @@
-# File: ./test/conftest.py
-
-import sys
-import os
-
-# 将项目根目录添加到 sys.path
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
-sys.path.insert(0, root_path)
